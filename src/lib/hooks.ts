939b712525c85b5e5a9b04a6aedbf00a8fa563ca
Andrew Vlietstra <vlietstra123@hotmail.com>
2024-12-21 15:29:38 -0500

    Made custom hook to close bookmarks popover when clicking outside popover or bookmark button elements

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -105,6 +105,23 @@ export const useLocalStorage = <T>(
 	return [value, setValue] as const;
 };
 
+export const useOnClickOutside = (
+	refs: React.RefObject<HTMLElement>[],
+	handler: () => void
+) => {
+	useEffect(() => {
+		const handleClick = (e: MouseEvent) => {
+			if (refs.every((ref) => !ref.current?.contains(e.target as Node))) {
+				handler();
+			}
+		};
+
+		document.addEventListener("click", handleClick);
+
+		return () => document.removeEventListener("click", handleClick);
+	}, [refs, handler]);
+};
+
 export const useBookmarksContext = () => {
 	const context = useContext(BookmarksContext);
 
